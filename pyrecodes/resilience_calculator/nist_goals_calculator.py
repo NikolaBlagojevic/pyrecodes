5e1eba758a9463c1ee8d19039eadeb1ac9feb774
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2024-12-26 17:37:30 -0800

    nist goals calculator bug fixed

--- pyrecodes/resilience_calculator/nist_goals_calculator.py
+++ pyrecodes/resilience_calculator/nist_goals_calculator.py
@@ -2,6 +2,11 @@ from pyrecodes.resilience_calculator.recodes_calculator import ReCoDeSCalculator
 import numpy as np
 
 class NISTGoalsCalculator(ReCoDeSCalculator):
+    """
+    | Resilience calculator class that assesses the resilience of a system based on NIST resilience goals.
+    | The NIST resilience goals are defined as achieving the desired functionality level of a system within a certain time after a disruption.
+    | Functionality level is defined as the ratio of the total consumption (i.e., met demand) to the total demand of a resource provided by the system.
+    """
 
     def __init__(self, resilience_goals: list) -> None:
         self.set_resilience_goals(resilience_goals)
@@ -23,19 +28,30 @@ class NISTGoalsCalculator(ReCoDeSCalculator):
             self.resilience_goals[-1]['GoalMet'] = []
 
     def update(self, system):
-        resources = system.resources
-        # compare demand and consumption and check if the goal is met
-        # if its met, save the time step in a bool list and then in the end in calculate resilience method get the first time step when the goal is met and maintained
+        """
+        | Method compares the demand and consumption of resources in the system and checks if the desired functionality level is met.
+        | A boolean list contains the information at which time steps the desired functionality level is met.
+        """
+        resources = system.resources        
         for resilience_goal in self.resilience_goals:
             resource_parameters = resources.get(resilience_goal['Resource'])            
             total_demand = resource_parameters['DistributionModel'].get_total_demand(scope=resilience_goal['Scope'])
             total_consumption = resource_parameters['DistributionModel'].get_total_consumption(scope=resilience_goal['Scope'])
-            resilience_goal['GoalMet'].append(resilience_goal['DesiredFunctionalityLevel'] < total_consumption/total_demand)
+            if total_demand == 0:
+                resilience_goal['GoalMet'].append(False)
+            else:
+                resilience_goal['GoalMet'].append(resilience_goal['DesiredFunctionalityLevel'] < total_consumption/total_demand)
 
     def calculate_resilience(self):
-        # find the time step at which the goal is met and MAINTAINED - the last time step at which the goal was not met
+        """"
+        | Method finds the time step at which the goal is met and MAINTAINED - the last time step at which the goal was not met.
+        | Since the demand and consumption of resources are updated at each time step, the goal may be met at one time step and not met at the next time step.
+        | If the goal is met at all time steps, the method returns 0.
+        """
         for resilience_goal in self.resilience_goals:
-            resilience_goal['MetAtTimeStep'] = np.where(np.asarray(resilience_goal['GoalMet']) == False)[0][-1] + 1
-            # remove GoalMet key not important anymore
+            if False in resilience_goal['GoalMet']:
+                resilience_goal['MetAtTimeStep'] = np.where(np.asarray(resilience_goal['GoalMet']) == False)[0][-1] + 1
+            else:
+                resilience_goal['MetAtTimeStep'] = 0
             del resilience_goal['GoalMet']
         return self.resilience_goals

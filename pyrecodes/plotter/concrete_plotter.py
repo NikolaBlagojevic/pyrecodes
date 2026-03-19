33d2ed66f6aa7b314c8656b3a579c9c17997dc68
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2026-03-19 12:54:35 -0700

    add household plotting methods to plotter class

--- pyrecodes/plotter/concrete_plotter.py
+++ pyrecodes/plotter/concrete_plotter.py
@@ -5,7 +5,7 @@ from pyrecodes.component.component import Component
 from pyrecodes.component.r2d_component import R2DComponent
 from pyrecodes.system.system import System
 from pyrecodes.resilience_calculator.recodes_calculator import ReCoDeSCalculator
-from pyrecodes.constants import ALL_RECOVERY_ACTIVITIES_LABELS, LOR_ALPHA, GANTT_BAR_DISTANCE, GANTT_BAR_WIDTH, ALL_RECOVERY_ACTIVITIES_COLORS
+from pyrecodes.constants import ALL_RECOVERY_ACTIVITIES_LABELS, LOR_ALPHA, GANTT_BAR_DISTANCE, GANTT_BAR_WIDTH, ALL_RECOVERY_ACTIVITIES_COLORS, HOUSEHOLD_GANTT_CHART_COLORS
 
 class ConcretePlotter():
     """
@@ -73,13 +73,13 @@ class ConcretePlotter():
 
     def plot_single_resource(self, time_steps: list, supply: list, demand: list, consumption: list,
                              axis_object: plt.axis, warmup=0, show=False, supply_label='Supply', demand_label='Demand',
-                             consumption_label='Consumption') -> None:
+                             consumption_label='Consumption', unmet_demand_label='Unmet Demand') -> None:
         [supply, demand, consumption] = self.add_warmup(warmup, [supply, demand, consumption])
         axis_object.plot(time_steps, supply, label=supply_label)
         axis_object.plot(time_steps, demand, label=demand_label)
         axis_object.plot(time_steps, consumption, label=consumption_label)
         axis_object.fill_between(time_steps, demand, consumption,
-                         label='LoR', alpha=LOR_ALPHA)
+                         label=unmet_demand_label, alpha=LOR_ALPHA)
         axis_object.legend(loc='lower right')
         if show:
             plt.show()
@@ -118,12 +118,14 @@ class ConcretePlotter():
         axis_object.set_yticklabels([component.__str__() for component in plotted_components])
         plt.show()
     
-    def set_gantt_chart_legend(self, recovery_activities_legend: list, axis_object: plt.axis) -> None:
+    def set_gantt_chart_legend(self, recovery_activities_legend: list, axis_object: plt.axis, legend_colors: dict = None) -> None:
         legend_labels = []
         legend_handles = []
+        colors = legend_colors if legend_colors is not None else ALL_RECOVERY_ACTIVITIES_COLORS
         for recovery_activity_name in set(recovery_activities_legend):
-            legend_labels.append(ALL_RECOVERY_ACTIVITIES_LABELS[recovery_activity_name])
-            legend_handles.append(plt.Rectangle((0, 0), 1, 1, fc=ALL_RECOVERY_ACTIVITIES_COLORS[recovery_activity_name]))
+            label = ALL_RECOVERY_ACTIVITIES_LABELS.get(recovery_activity_name, recovery_activity_name)
+            legend_labels.append(label)
+            legend_handles.append(plt.Rectangle((0, 0), 1, 1, fc=colors[recovery_activity_name]))
         axis_object.legend(legend_handles, legend_labels, loc='upper left', bbox_to_anchor=(1.02, 1.0))
 
     def get_component_recovery_progress(self, component: Component) -> dict:
@@ -199,5 +201,92 @@ class ConcretePlotter():
         with open(savename, mode="w") as file:
             json.dump(component_recovery_progress, file)
 
-            
+    def plot_household_movement(self, components: list, axis_object: plt.axis) -> None:
+        all_households = self.collect_all_households(components)
+        locations = []
+        household_ids = []
+        for household_count, household in enumerate(all_households):
+            household_ids.append(household.id)
+            locations += self.plot_single_household_movement(household, household_count, axis_object)
+        self.set_gantt_chart_legend(locations, axis_object, HOUSEHOLD_GANTT_CHART_COLORS)
+        axis_object.set_yticks([household_count for household_count in range(len(household_ids))])
+        axis_object.set_yticklabels([household_id for household_id in household_ids])
+        plt.show()
+
+    def plot_single_household_movement(self, household, household_count: int, axis_object: plt.axis, bar_width=0.5) -> list:
+        locations = []
+        for time_step, location in enumerate(household.staying_at):
+            axis_object.broken_barh([(time_step, 1)], (household_count-bar_width*0.5, bar_width),
+                                    facecolors=HOUSEHOLD_GANTT_CHART_COLORS[location],
+                                    edgecolor="none")
+            locations.append(location)
+        return locations
+
+    def collect_all_households(self, components: list) -> list:
+        all_households = []
+        for component in components:
+            all_households += getattr(component, "households", [])
+        return all_households
+
+    def get_household_location_counts(self, components: list) -> dict:
+        all_households = self.collect_all_households(components)
+        if not all_households:
+            return {}
+        time_length = len(all_households[0].staying_at)
+        counts = {loc: [0] * time_length for loc in HOUSEHOLD_GANTT_CHART_COLORS}
+        for household in all_households:
+            for time_step, location in enumerate(household.staying_at):
+                if location in counts:
+                    counts[location][time_step] += 1
+        return counts
+
+    def plot_household_location_over_time(self, components: list, x_axis_label='Time step [day]') -> None:
+        counts = self.get_household_location_counts(components)
+        if not counts:
+            return
+        time_steps = list(range(len(counts['Home'])))
+        axis_object = self.setup_lor_plot_fig(x_axis_label, 'Number of households in different locations')
+        layers = [('Home', 'At Home'), ('Friend', 'With Friend'), ('OutOfTown', 'Out of Town')]
+        cividis = mpl.colormaps['cividis']
+        colors = [cividis(i / (len(layers) - 1)) for i in range(len(layers))]
+        bottoms = [0] * len(time_steps)
+        for (key, label), color in zip(layers, colors):
+            tops = [b + v for b, v in zip(bottoms, counts[key])]
+            axis_object.fill_between(time_steps, bottoms, tops, label=label, alpha=0.5, color=color)
+            bottoms = tops
+        self.reorder_legend(['Out of Town', 'With Friend', 'At Home'], loc='lower right')
+        plt.show()
+
+    def reorder_legend(self, order: list, ax=None, **legend_kwargs) -> None:
+        if ax is None:
+            ax = plt.gca()
+        handles, labels = ax.get_legend_handles_labels()
+        pairs = dict(zip(labels, handles))
+        ordered = [(pairs[l], l) for l in order if l in pairs]
+        if ordered:
+            ordered_handles, ordered_labels = zip(*ordered)
+            ax.legend(ordered_handles, ordered_labels, **legend_kwargs)
         
+    def get_met_demand_over_time(self, components: list, resource_name: str) -> list:
+        met_demand_tracker = getattr(components[0], 'met_demand_tracker', None)
+        if met_demand_tracker is None:
+            return []
+        time_length = len(met_demand_tracker)
+        totals = [0.0] * time_length
+        for component in components:
+            last_value = 0.0
+            for time_step, met_demand in enumerate(getattr(component, 'met_demand_tracker', [])):
+                if resource_name in met_demand:
+                    last_value = met_demand[resource_name]['PercentOfMetDemand']
+                totals[time_step] += last_value
+        return totals
+
+    def plot_met_demand_over_time(self, components: list, resource_name: str, x_axis_label='Time step [day]') -> None:
+        totals = self.get_met_demand_over_time(components, resource_name)
+        if not totals:
+            return
+        time_steps = list(range(len(totals)))
+        axis_object = self.setup_lor_plot_fig(x_axis_label, f'{resource_name} met demand')
+        axis_object.fill_between(time_steps, 0, totals, alpha=0.5, label='Met demand')
+        axis_object.legend(loc='lower right')
+        plt.show()
